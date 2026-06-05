2b765af227610c02b01aa1e5e8896a9014f980d1
michellestrey <micastrey@gmail.com>
2026-06-05 18:13:53 -0300

    Criação dos métodos no controller

--- src/main/java/com/api/status/order_api/controller/OrderController.java
+++ src/main/java/com/api/status/order_api/controller/OrderController.java
@@ -2,13 +2,18 @@ package com.api.status.order_api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.status.order_api.dto.CreateOrderDTO;
 import com.api.status.order_api.dto.ResponseOrderDTO;
+import com.api.status.order_api.dto.UpdateOrderDTO;
 import com.api.status.order_api.service.OrderService;
 
 @RestController
@@ -29,7 +34,23 @@ public class OrderController {
 						.status(HttpStatus.CREATED)
 						.body(response);
 	}
-
-	
-
+    @GetMapping("/{id}")
+	public ResponseEntity<ResponseOrderDTO> getById(@PathVariable Long id){
+    	return ResponseEntity.ok(orderService.buscarPorId(id));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<ResponseOrderDTO> update(@PathVariable Long id, 
+    		                                      @RequestBody UpdateOrderDTO dto){
+    	return ResponseEntity.ok(orderService.atualizar(id, dto));
+    	
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+    	orderService.deletar(id);
+    	return ResponseEntity.noContent().build();
+    	}
+    
+    // IMPLEMENTAR PATCH NO FUTURO
 }
